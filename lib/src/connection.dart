5332d83775f308026395f5b1b8b74e8c5b04bc45
Wilson Borba <asasdeaguia10@gmail.com>
2025-01-27 13:51:00 +0700

    feat: improved some function to be acessible

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -1,47 +1,53 @@
-
-
 import 'package:postgres/postgres.dart';
 
 class SQLConnection {
-
   final String host;
-  final String port;
+  final int port;
+  final String database;
   final String user;
   final String password;
   final ConnectionSettings settings;
 
-  SQLConnection(
-    {
-    required this.host, 
-    required this.port, 
-    required this.user, 
-    required this.password, 
+  SQLConnection({
+    required this.host,
+    required this.port,
+    required this.database,
+    required this.user,
+    required this.password,
     this.settings = const ConnectionSettings(sslMode: SslMode.disable),
-    }
-    
+  });
+
+  late Connection _connection;
+
+  Future<void> open() async {
+    final endpoint = Endpoint(
+      host: host,
+      port: port,
+      database: database,
+      username: user,
+      password: password,
     );
 
-    
-    Future<Connection> open() async {
-      // The postgres server hosted locally doesn't have SSL by default. If you're
-      // accessing a postgres server over the Internet, the server should support
-      // SSL and you should swap out the mode with `SslMode.verifyFull`.
-      
-      final conn = await Connection.open(
-            Endpoint(
-              host: 'localhost',
-              database: 'postgres',
-              username: 'user',
-              password: 'pass',
-            ),
-            settings: settings,
-          );
+    _connection = await Connection.open(endpoint, settings: settings);
+  }
 
-      return conn;
+  Future<List<Map<String, dynamic>>>  query(String sql, {
+      Object? parameters,
+      bool ignoreRows = false,
+      QueryMode? queryMode,
+      Duration? timeout,
+    }) async {
 
-    }
+     final result = await _connection.execute(sql, 
+     parameters: parameters, 
+     ignoreRows: ignoreRows, queryMode: queryMode, timeout: timeout
+     
+     );
+    return result.map((row) => row.toColumnMap()).toList();
+  }
+  
 
-
-
-
+  Future<void> close() async {
+    await _connection.close();
+  }
 }
